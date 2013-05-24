4dc3882b99f37fdc16a3f51ec0e886692a0fdcac
dsanz <daniel.sanz@liferay.com>
2013-05-24 03:04:58 -0700

    improve properties API by encapsulating access to translations array. Use this new api from now on

--- src/api/api-properties.sh
+++ src/api/api-properties.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
 # T contains all translations
-declare -A T;
+declare -gA T;
 # K contains all keys in the template file
-declare -a K;
+declare -ga K;
 
 # regexp for separating key/value pairs
-declare kv_rexp="^([^=]+)=(.*)"
+declare -g kv_rexp="^([^=]+)=(.+)$"
 
 #### Base functions
+
+# returns a key which can be used to access the associative array T
+# $1 is the key prefix
+# $2 is the language key we want to access
+function getTKey() {
+    echo $1$2
+}
+
+# returns a value obtained from accessing the array T using the given key
+# $1 is the key prefix
+# $2 is the language key we want to access
+function getTVal() {
+    k=$(getTKey $1 $2)
+    echo ${T[$k]}
+}
+
+# sets the specified value into array T under given key
+# $1 is the key prefix
+# $2 is the language key we want to access
+# $3 is the value
+function setTVal() {
+    k=$(getTKey $1 $2)
+    T[$k]="$3"
+}
 
 # removes from T all entries for a given prefix
 # $1 is the prefix
 function clear_keys() {
     for key in "${K[@]}"; do
-		unset 'T[$1,$key]'
+		unset T[$(getTKey $1 $key)]
 	done;
 }
 # returns true if line is a translation line (as opposed to comments or blank lines), false otherwise
@@ -26,32 +50,40 @@ function is_key_line() {
 # $1 is the key prefix
 # $2 is the key name
 function exists_key() {
-	[ ${T[$1,$2]+abc} ]
+	[ $($(getTVal $1 $2)+abc) ]
 }
 # returns true if value of a given key has changed amongst 2 key prefixes, false otherwise
 # $1 is one key prefix
 # $2 is the other key prefix
 # $3 is the key name
 function value_changed() {
-	[[ ${T[$1,$3]} != ${T[$2,$3]} ]]
+	[[ $(getTVal $1 $3) != $(getTVal $2 $3) ]]
+}
+
+function is_translated_value() {
+	rexp='\(Automatic [^\)]+\)$'
+	! [[ "$1" =~ $rexp ]]
 }
 
 function is_translated() {
-	rexp='\(Automatic [^\)]+\)$'
-	! [[ "${T[$1,$2]}" =~ $rexp ]]
+	is_translated_value "$(getTVal $1 $2)"
 }
 function is_automatic_copy() {
 	rexp='\(Automatic Copy\)$'
-	[[ "${T[$1,$2]}" =~ $rexp ]]
+	[[ "$(getTVal $1 $2)" =~ $rexp ]]
 }
 function is_automatic_translation() {
 	rexp='\(Automatic Translation\)$'
-	[[ "${T[$1,$2]}" =~ $rexp ]]
+	[[ "$(getTVal $1 $2)" =~ $rexp ]]
 }
 
 function get_locale_from_file_name() {
     file=$1
-    echo $file | sed -r 's/Language_([^\.]+)\.properties/\1/'
+    if [[ $file == "$FILE.$PROP_EXT" ]]; then
+        echo "template"
+    else
+        echo $file | sed -r 's/Language_([^\.]+)\.properties/\1/'
+    fi
 }
 
 # reads a file and inserts keys in T (also in K if applicable)
@@ -69,7 +101,7 @@ function read_locale_file() {
 		(( counter++ ))
 		if is_key_line "$line" ; then
 			[[ "$line" =~ $kv_rexp ]] && key="${BASH_REMATCH[1]}" && value="${BASH_REMATCH[2]}"
-			T[$2,$key]=$value
+			setTVal $2 "$key" "$value"
 			if [[ $template ]]; then
 				K[${#K[@]}]=$key
 			fi;
