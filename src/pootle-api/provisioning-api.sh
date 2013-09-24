da8deb71a249e46c02393136ec06c9aca11f6967
Daniel Sanz <daniel.sanz@liferay.com>
2013-09-24 15:39:46 -0700

    fix bad field numbering

--- src/pootle-api/provisioning-api.sh
+++ src/pootle-api/provisioning-api.sh
@@ -10,10 +10,10 @@ function create_pootle_project() {
     logt 3 -n "Posting new project form"
     curl -s -b "$PO_COOKIES" -c "$PO_COOKIES"  -d "csrfmiddlewaretoken=`cat ${PO_COOKIES} | grep csrftoken | cut -f7`"\
         -d "form-TOTAL_FORMS=1"  -d "form-INITIAL_FORMS=0" -d "changeprojects=Save Changes"\
-        -d "form-1-id=" -d "form-1-code:$projectCode" -d "ºform-1-fullname=$projectName"\
-        -d "form-1-checkstyle=standard" -d "form-1-localfiletype=properties" -d "form-1-treestyle=gnu" \
-        -d "form-1-source_language=2" -d "form-1-ignoredfiles=" -d "changeprojects:Save Changes"\
-        "$PO_SRV$path/admin/projects.html" > /dev/null
+        -d "form-0-id=" -d "form-0-code=$projectCode" -d "form-0-fullname=$projectName"\
+        -d "form-0-checkstyle=standard" -d "form-0-localfiletype=properties" -d "form-0-treestyle=gnu" \
+        -d "form-0-source_language=2" -d "form-0-ignoredfiles=" -d "changeprojects:Save Changes"\
+        "$PO_SRV$path/admin/projects.html" > /home/dsanz/out.html #> /dev/null
     check_command
 	close_pootle_session
 }
