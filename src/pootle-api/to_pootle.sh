ea627c9cb8527f9ec97ac15c98d747e18d587550
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-22 06:51:15 -0700

    add function to read the derived language file, pretty similar to the others

--- src/pootle-api/to_pootle.sh
+++ src/pootle-api/to_pootle.sh
@@ -149,3 +149,17 @@ function post_language_translations() {
 	generate_additions
 	post_new_translations
 }
+
+# given a project and a language, reads the Language_xx.properties file
+# present in current directory puts it into array T using the locale as prefix
+function read_derived_language_file() {
+    project="$1";
+    locale="$2";
+    langFile="$FILE$LANG_SEP$locale.$PROP_EXT"
+	prefix=$(get_derived_language_prefix $project $locale)
+	read_locale_file $langFile $prefix
+}
+
+function get_derived_language_prefix() {
+    echo d$1$2
+}
