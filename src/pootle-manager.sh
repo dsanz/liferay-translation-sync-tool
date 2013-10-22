754debb63b7dd26bc61ad71841bba641533e3643
Daniel Sanz <daniel.sanz@liferay.com>
2013-10-22 07:00:24 -0700

    add db backup before publishing anything

--- src/pootle-manager.sh
+++ src/pootle-manager.sh
@@ -127,6 +127,7 @@ function backport_all() {
 
 function upload_translations() {
     loglc 1 $RED  "Uploading $2 translations for project $1"
+    backup_db
     post_file $1 $2
     loglc 1 $RED "Upload finished"
 }
@@ -136,9 +137,8 @@ function upload_derived_translations() {
     derived_locale="$2"
     parent_locale="$3"
     loglc 1 $RED  "Uploading $derived_locale (derived language) translations for project $project"
-
+    backup_db
     post_derived_translations $project $derived_locale $parent_locale
-
     loglc 1 $RED "Upload finished"
 }
 
