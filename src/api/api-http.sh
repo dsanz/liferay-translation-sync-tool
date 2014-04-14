1916ced5d8446b76b0c4d0eb3630982d869ad0a5
Daniel Sanz <daniel.sanz@liferay.com>
2014-04-14 13:21:37 +0200

    change all curl invocations to reuse CURL_OPTS variable

--- src/api/api-http.sh
+++ src/api/api-http.sh
@@ -3,7 +3,7 @@
 function close_pootle_session() {
 	# get logout page and delete cookies
 	logt 3 -n "Closing pootle session... "
-	curl -s -b "$PO_COOKIES" -c "$PO_COOKIES" "$PO_SRV/accounts/logout" > /dev/null
+	curl $CURL_OPTS "$PO_SRV/accounts/logout"
 	check_command
 }
 
@@ -11,10 +11,10 @@ function start_pootle_session() {
 	close_pootle_session
 	# 1. get login page (and cookies)
 	logt 3 -n "Accessing Pootle login page... "
-	curl -s -b "$PO_COOKIES" -c "$PO_COOKIES" "$PO_SRV/accounts/login" > /dev/null
+	curl $CURL_OPTS "$PO_SRV/accounts/login"
 	check_command
 	# 2. post credentials, including one received cookie
 	logt 3 -n "Authenticating as '$PO_USER' "
-	curl -s -b "$PO_COOKIES" -c "$PO_COOKIES" -d "username=$PO_USER;password=$PO_PASS;csrfmiddlewaretoken=`cat ${PO_COOKIES} | grep csrftoken | cut -f7`" "$PO_SRV/accounts/login" > /dev/null
+	curl $CURL_OPTS -d "username=$PO_USER;password=$PO_PASS;csrfmiddlewaretoken=`cat ${PO_COOKIES} | grep csrftoken | cut -f7`" "$PO_SRV/accounts/login"
 	check_command
 }
