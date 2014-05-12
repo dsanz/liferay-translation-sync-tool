f82412fbcae29ce8c943060b73317160d0b0de0f
Daniel Sanz <daniel.sanz@liferay.com>
2014-05-12 16:19:04 +0200

    add function for restoring a backup. Make sure pootle filesystem gets compressed without trailing dirs. Still needed to stop/start the server

--- src/api/api-db.sh
+++ src/api/api-db.sh
@@ -21,10 +21,55 @@ function backup_db() {
 
 	logt 2 "Compressing po/ dir into $fsfilepath"
 	logt 3 -n "Running tar command: tar czvf $fsfilepath $PODIR";
-	tar czvf $fsfilepath $PODIR > /dev/null 2>&1;
+	old_pwd=$(pwd)
+	cd $PODIR
+	tar czvf $fsfilepath * > /dev/null 2>&1;
+	cd $old_pwd
 	check_command;
 
 	logt 2 "Backup ID: $backup_dir"
+}
+
+function restore_db() {
+	backup_dir="$1"
+	logt 1  "Restoring pootle data from backup ID: $backup_dir"
+	base_dir=$(echo "$backup_dir" | cut -d "-" -f 1-2)
+	dumpfilename="pootle.sql"
+	bzippeddumpfilename="pootle.sql.bz2"
+	dumpfilepath="$TMP_DB_BACKUP_DIR/$base_dir/$backup_dir/$dumpfilename";
+	bzippeddumpfilepath="$TMP_DB_BACKUP_DIR/$base_dir/$backup_dir/$bzippeddumpfilename";
+	fsfilename="po.tgz"
+	fsfilepath="$TMP_DB_BACKUP_DIR/$base_dir/$backup_dir/$fsfilename";
+
+	logt 2  "Restoring pootle database"
+	logt 3 -n "Decompressing db dump";
+	bunzip2 -k $bzippeddumpfilepath > /dev/null 2>&1;
+	check_command;
+
+	clean_tables
+	logt 3 -n "Restoring DB dump";
+	cat $dumpfilepath | $MYSQL_COMMAND $DB_NAME
+	check_command;
+
+	logt 2  "Restoring pootle filesystem"
+	clean_dir $PODIR
+	logt 3  "Decompressing filesystem backup"
+	tar xf $fsfilepath -C $PODIR > /dev/null 2>&1;
+	check_command;
+
+	logt 2 "Cleaning files"
+	rm $dumpfilepath;
+}
+
+function clean_tables() {
+	logt 3 "Cleaning DB tables";
+	drop_table_sentences=$($MYSQL_DUMP_COMMAND $DB_NAME --add-drop-table --no-data | grep "^DROP")
+	for drop_table in $drop_table_sentences;
+	do
+		logt 4 -n "$drop_table"
+		$MYSQL_COMMAND $DB_NAME -s -e "$drop_table";
+		check_command
+	done
 }
 
 # given the storeId and the language key (unitId) returns the index of that translation unit in the DB
