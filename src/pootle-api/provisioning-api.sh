8da4270ccdfd562d9a0b5e42829d727252383a57
Daniel Sanz <daniel.sanz@liferay.com>
2014-06-10 16:48:38 +0200

    minor change in project provisioning post

--- src/pootle-api/provisioning-api.sh
+++ src/pootle-api/provisioning-api.sh
@@ -9,10 +9,10 @@ function create_pootle_project() {
 	# this creates the pootle project
 	logt 3 -n "Posting new project form"
 	curl $CURL_OPTS -d "csrfmiddlewaretoken=`cat ${PO_COOKIES} | grep csrftoken | cut -f7`"\
-        -d "form-TOTAL_FORMS=1"  -d "form-INITIAL_FORMS=0" -d "changeprojects=Save Changes"\
+        -d "form-TOTAL_FORMS=1" -d "form-INITIAL_FORMS=0" -d "form-MAX_NUM_FORMS=1000"\
         -d "form-0-id=" -d "form-0-code=$projectCode" -d "form-0-fullname=$projectName"\
         -d "form-0-checkstyle=standard" -d "form-0-localfiletype=properties" -d "form-0-treestyle=gnu" \
-        -d "form-0-source_language=2" -d "form-0-ignoredfiles=" -d "changeprojects:Save Changes"\
+        -d "form-0-source_language=2" -d "form-0-ignoredfiles=" -d "changeprojects=Save Changes"\
         "$PO_SRV$path/admin/projects.html"
 	check_command
 	close_pootle_session
