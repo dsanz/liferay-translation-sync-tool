98486c189130184442fca4c5da9c5df6a88090bc
dsanz <daniel.sanz@liferay.com>
2014-09-18 16:36:41 +0200

    define colors only if so configured

--- src/api/api-config.sh
+++ src/api/api-config.sh
@@ -233,9 +233,10 @@ function print_action() {
 }
 
 function set_colors() {
-	# some colours
-	COLOROFF="\033[1;0m"; GREEN="\033[1;32m"; RED="\033[1;31m"; LILA="\033[1;35m"
-	YELLOW="\033[1;33m"; BLUE="\033[1;34m"; WHITE="\033[1;37m"; CYAN="\033[1;36m"
+	if [[ "${LR_TRANS_MGR_COLOR_LOG}x" == "1x" ]]; then
+		COLOROFF="\033[1;0m"; GREEN="\033[1;32m"; RED="\033[1;31m"; LILA="\033[1;35m"
+		YELLOW="\033[1;33m"; BLUE="\033[1;34m"; WHITE="\033[1;37m"; CYAN="\033[1;36m"
+	fi
 }
 
 function load_config() {
