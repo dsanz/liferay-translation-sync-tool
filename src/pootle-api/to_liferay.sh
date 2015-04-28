db89d210769a0256f584734bc14dedf0c406edc0
dsanz <daniel.sanz@liferay.com>
2015-04-28 09:09:09 +0200

    make pull-request link available through the logs

--- src/pootle-api/to_liferay.sh
+++ src/pootle-api/to_liferay.sh
@@ -97,8 +97,9 @@ function submit_pull_request() {
 	logt 3 -n "Pushing remote branch origin/$EXPORT_BRANCH"
 	git push origin "$EXPORT_BRANCH" > /dev/null 2>&1
 	logt 3 -n "Sending pull request to $PR_REVIEWER"
-	$HUB_BIN pull-request -m "Translations from pootle. Automatic PR sent by $product" -b "$PR_REVIEWER":master -h $EXPORT_BRANCH > /dev/null 2>&1
+	pr_url=$($HUB_BIN pull-request -m "Translations from pootle. Automatic PR sent by $product" -b "$PR_REVIEWER":master -h $EXPORT_BRANCH)
 	check_command
+	logt 4 -n "Pull request URL: $pr_url"
 	logt 3 -n "git checkout master"
 	git checkout master > /dev/null 2>&1
 	check_command
