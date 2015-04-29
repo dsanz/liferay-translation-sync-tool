c29d452541a709b399fd54c9c67701c5513f504d
dsanz <daniel.sanz@liferay.com>
2015-04-29 09:57:43 +0200

    SF rename source_base_path to git_root_dir to align names

--- src/api/api-projects.sh
+++ src/api/api-projects.sh
@@ -8,27 +8,27 @@ function add_git_root() {
 
 # Adds a new project to the project arrays. Requires 4 parameters
 #  - project name (eg "sibboleth-hook")
-#  - source base path: root of source code for that project (git root dir)
+#  - git_root_dir: root of source code for that project
 #  - lang rel path: path where Language.properties file lives, relative to $2
 #  - ant rel path: path where ant build-lang has to be invoked, relative to $2
 function add_project() {
 	project_name="$1"
-	source_base_path="$2"
+	git_root_dir="$2"
 	lang_rel_path="$3"
 	ant_rel_path="$4"
 
 	PROJECT_NAMES["$project_name"]="$project_name"
-	PROJECT_SRC_LANG_BASE["$project_name"]="$source_base_path$lang_rel_path"
-	PROJECT_ANT_BUILD_LANG_DIR["$project_name"]="$source_base_path$ant_rel_path"
-	PATH_BASE_DIR["$source_base_path"]="$source_base_path"
-	PATH_PROJECTS["$source_base_path"]=" $project_name"${PATH_PROJECTS["$source_base_path"]}
+	PROJECT_SRC_LANG_BASE["$project_name"]="$git_root_dir$lang_rel_path"
+	PROJECT_ANT_BUILD_LANG_DIR["$project_name"]="$git_root_dir$ant_rel_path"
+	PATH_BASE_DIR["$git_root_dir"]="$git_root_dir"
+	PATH_PROJECTS["$git_root_dir"]=" $project_name"${PATH_PROJECTS["$git_root_dir"]}
 }
 
 # adds a bunch of projects to the project arrays. This function is specific for
 # adding liferay plugins stored in the same git repo. Requires 4 parameters:
 #  - project names list: a space-separated string of project names, w/o suffix
 #  - type: indicate the Liferay plugin type ("hook", "portlet", "theme")
-#  - source_base_path: root of source code for the plugins SDK/repo
+#  - git_root_dir: root of source code for the plugins SDK/repo
 #  - lang_rel_path: path where Language.properties file lives (relative to ${3}/project_rel_path/project_name).
 #  - [optional] project_rel_path: relative to $3, indicates the dir where project is stored. If not specified, Liferay SDK dir layout will be used
 # Function will compute the actual paths for each individual project as they are laid out in a SDK/plugins git repo
@@ -50,40 +50,40 @@ function add_projects_Liferay_plugins() {
 	fi;
 
 
-	source_base_path="$3"
+	git_root_dir="$3"
 	lang_rel_path_fragment="$4"
 
 	for name in $project_names;
 	do
 		# project name is made up from the name + project_type (eg "mail" + "-portlet")
 		project_name="$name$project_type"
-		# project base path locates the project inside source_base_path (e.g "portlets/mail-portlet/")
+		# project base path locates the project inside git_root_dir (e.g "portlets/mail-portlet/")
 		project_base_path="$project_rel_path_fragment${project_name}/"
 		# ant path is assumed to be the project base path. Works for our SDK plugins
 		# lang_rel_path is prefixed with the project dir
 		lang_rel_path="$project_base_path$lang_rel_path_fragment"
-		add_project "$project_name" "${source_base_path}" "$lang_rel_path" "$project_base_path"
+		add_project "$project_name" "${git_root_dir}" "$lang_rel_path" "$project_base_path"
 	done
 }
 
 # adds a bunch of projects to the project arrays. This function is generic and allows to
 # add any project set stored in the same git repo. Requires 3 parameters:
 #  - project names list: a space-separated string of project names, w/o suffix
-#  - source_base_path: root of source code for the plugins SDK/repo
+#  - git_root_dir: root of source code for the plugins SDK/repo
 #  - lang_rel_path: path where Language.properties file lives (relative to ${2}/).
 # Function assumes thst ant path is the root of each project
 function add_projects() {
 	project_names="$1"
-	source_base_path="$2"
+	git_root_dir="$2"
 	lang_rel_path_fragment="$3"
 
 	for project_name in $project_names;
 	do
-		# project base path locates the project inside source_base_path (e.g "portlets/mail-portlet/")
+		# project base path locates the project inside git_root_dir (e.g "portlets/mail-portlet/")
 		project_base_path="$project_rel_path_fragment${project_name}/"
 		# ant path is assumed to be the project base path. Works for our SDK plugins
 		# lang_rel_path is prefixed with the project dir
 		lang_rel_path="$project_name/$lang_rel_path_fragment"
-		add_project "$project_name" "${source_base_path}" "$lang_rel_path" "$project_name"
+		add_project "$project_name" "${git_root_dir}" "$lang_rel_path" "$project_name"
 	done
 }
