5905f81a3b621cfa8034b8a2118de1e8c6d861e7
dsanz <daniel.sanz@liferay.com>
2015-05-04 18:16:07 +0200

    generate diffs on a per-file basis. This allows sync tool to commit its results only for a few projects/files and not loosing translations committed by developers

--- src/pootle-api/to_pootle.sh
+++ src/pootle-api/to_pootle.sh
@@ -29,71 +29,65 @@ function clean_temp_input_dirs() {
 }
 
 function generate_addition() {
-	path="$1"
-	project="$2"
-	cd $path > /dev/null 2>&1
-	files="$(ls ${FILE}${LANG_SEP}*.${PROP_EXT} 2>/dev/null)"
-	for file in $files; do
-		if [[ "$file" != "${FILE}${LANG_SEP}en.${PROP_EXT}" ]]; then
-			git diff $LAST_BRANCH $file | sed -r 's/^[^\(]+\(Automatic [^\)]+\)$//' | grep -E "^\+[^=+][^=]*" | sed 's/^+//g' > $TMP_PROP_IN_DIR/$project/$file
-			number_of_additions=$(cat "$TMP_PROP_IN_DIR/$project/$file" | wc -l)
-			color="$WHITE"
-			if [[ $number_of_additions -eq 0 ]]; then
-				rm "$TMP_PROP_IN_DIR/$project/$file"
-				color="$COLOROFF"
-			fi;
-			loglc 0 "$color" -n $(get_locale_from_file_name $file)"($number_of_additions) "
+	project="$1"
+	path="$2"
+	language_file="$path/$3"
+	commit="$4"
+
+	if [[ "$language_file" != "${FILE}${LANG_SEP}en.${PROP_EXT}" ]]; then
+		logt 5 -n "Generating additions from: git diff $commit $file "
+		git diff $commit $file | sed -r 's/^[^\(]+\(Automatic [^\)]+\)$//' | grep -E "^\+[^=+][^=]*" | sed 's/^+//g' > $TMP_PROP_IN_DIR/$project/$file
+		number_of_additions=$(cat "$TMP_PROP_IN_DIR/$project/$file" | wc -l)
+		color="$WHITE"
+		if [[ $number_of_additions -eq 0 ]]; then
+			rm "$TMP_PROP_IN_DIR/$project/$file"
+			color="$COLOROFF"
+		fi;
+			loglc 0 "$color" -n $(get_locale_from_file_name $language_file)"($number_of_additions) "
 		fi
 	done;
 	log ""
 }
 
-function create_branch_at_child_of_last_export_commit() {
+function get_last_export_commit() {
 	# assume we are now in master
 	base_src_dir="$1"
-	logt 3 "Creating branch at child of last export commit"
-	logt 4 "Searching git history for last export commit"
+	path="$2"
+	language_file="$path/$3"
+
+	msg="$language_file: "
 	cd $base_src_dir
 	child_of_last_export="HEAD"
-	last_export_commit=$(git log -n 1 --grep "$product_name" --after 2012 --format=format:"%H")
+	last_export_commit=$(git log -n 1 --grep "$product_name" --after 2012 --format=format:"%H" $language_file)
 	if [[ $last_export_commit == "" ]]; then
-		logt 5 "I couldn't find a previous export commit containing $product_name"
-		last_export_commit=$(git log -n 1 --grep "$old_product_name" --after 2012 --format=format:"%H")
+		msg="$msg (no export commit containing $product_name) "
+		last_export_commit=$(git log -n 1 --grep "$old_product_name" --after 2012 --format=format:"%H" $language_file)
 	fi;
 	if [[ $last_export_commit == "" ]]; then
-		logt 5 "I couldn't find a previous export commit containing $old_product_name"
+		msg="$msg (no export commit containing $product_name) "
 	else
 		child_of_last_export=$(git rev-list --children --after 2012 HEAD | grep "^$last_export_commit" | cut -f 2 -d ' ')
 	fi;
-	last_export_msg=$(git show --pretty=oneline --abbrev-commit ${child_of_last_export}^ | head -n 1)
-	logt 4 "Using $child_of_last_export as reference (parent commit is: $last_export_msg)"
-	logt 4 "Setting up $LAST_BRANCH branch pointing to $child_of_last_export"
-	if exists_branch $LAST_BRANCH $base_src_dir; then
-		logt 5 -n "git branch -D $LAST_BRANCH";
-		git branch -D $LAST_BRANCH  > /dev/null 2>&1
-		check_command
-	fi;
-	logt 5 -n "git branch $LAST_BRANCH $child_of_last_export";
-	git branch $LAST_BRANCH $child_of_last_export
-	check_command
+	msg="$msg using child_of_last_export"
+	logt 4 "$msg"
+	$child_of_last_export;
 }
 
 function generate_additions() {
-	logt 1 "Calculating committed translations from last export commit"
+	logt 1 "Calculating committed translations from latest export commit, for each project/language"
 	for base_src_dir in "${!GIT_ROOTS[@]}"; do
 		projects="${PROJECTS_BY_GIT_ROOT["$base_src_dir"]}"
 		cd $base_src_dir
 		logt 2 "$base_src_dir"
-		create_branch_at_child_of_last_export_commit "$base_src_dir"
-		if exists_branch $LAST_BRANCH $base_src_dir; then
-			for project in $projects; do
-				logt 3 "$project"
-				path="${PROJECT_SRC_LANG_BASE["$project"]}"
-				generate_addition "$path" "$project"
-			done;
-		else
-			logt 3 "There is no '$LAST_BRANCH' branch, so I can't diff it with master to detect additions for projects $projects"
-		fi;
+		for project in $projects; do
+			logt 3 "$project"
+			path="${PROJECT_SRC_LANG_BASE["$project"]}"
+			cd $path > /dev/null 2>&1
+			for language_file in $(ls ${FILE}${LANG_SEP}*.$PROP_EXT 2>/dev/null); do
+				commit=$(get_last_export_commit "$base_src_dir" "$path" "$language_file")
+				generate_addition "$project" "$path" "$language_file" "$commit"
+			done
+		done;
 	done;
 }
 
