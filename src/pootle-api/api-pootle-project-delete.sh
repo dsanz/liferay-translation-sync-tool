eae25e3d57f0a6c3304749d040d221c31400efc7
dsanz <daniel.sanz@liferay.com>
2015-07-07 16:03:57 +0200

    fine tune post parameters for project deletion

--- src/pootle-api/api-pootle-project-delete.sh
+++ src/pootle-api/api-pootle-project-delete.sh
@@ -29,10 +29,17 @@ function delete_pootle_project() {
 	curl $CURL_OPTS -d "csrfmiddlewaretoken=`cat ${PO_COOKIES} | grep csrftoken | cut -f7`"\
         -d "form-TOTAL_FORMS=1" -d "form-INITIAL_FORMS=0" -d "form-MAX_NUM_FORMS=1000"\
         -d "form-0-id=$id" -d "form-0-code=$projectCode" -d "form-0-fullname=$projectName"\
-        -d "form-0-checkstyle=standard" -d "form-0-localfiletype=properties" -d "form-0-treestyle=gnu" \
+        -d "form-0-checkstyle=standard" -d "form-0-localfiletype=properties" \
         -d "form-0-source_language=2" -d "form-0-ignoredfiles=" -d "form-0-DELETE=on" \
         -d "changeprojects=Save Changes" \
         "$PO_SRV$path/admin/projects.html"
 	check_command
 	close_pootle_session
 }
+#        form-9-id:102
+#        form-9-code:kk
+#        form-9-fullname:de Vaca
+#        form-9-checkstyle:standard
+#        form-9-source_language:2
+#        form-9-ignoredfiles:
+#        form-9-DELETE:on
