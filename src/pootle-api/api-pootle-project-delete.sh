86c04646b4049e6112f212971486b588038540e9
dsanz <daniel.sanz@liferay.com>
2015-07-15 12:15:31 +0200

    make delete_pootle_project configurable to open a pootle session or not

--- src/pootle-api/api-pootle-project-delete.sh
+++ src/pootle-api/api-pootle-project-delete.sh
@@ -1,11 +1,12 @@
 # top level function to delete a project in pootle
 function delete_project_in_pootle() {
 	projectCode="$1"
+	open_session="$2"
 
 	if is_pootle_server_up; then
 		if exists_project_in_pootle "$1"; then
 			logt 1 "Deleting project '$projectCode' in pootle"
-			delete_pootle_project $projectCode
+			delete_pootle_project $projectCode $open_session
 		else
 			logt 1 "Pootle project '$projectCode' does not exist. Aborting..."
 		fi
@@ -17,9 +18,14 @@ function delete_project_in_pootle() {
 
 function delete_pootle_project() {
 	projectCode="$1"
+	open_session="$2"
 
 	logt 2 "Deleting project $projectCode from pootle server"
-	start_pootle_session
+	if [[ ${open_session+1} ]]; then
+		logt 3 "Reusing existing pootle session"
+	else
+		start_pootle_session
+	fi;
 
 	id="$(get_pootle_project_id_from_code $projectCode)"
 	projectName="$(get_pootle_project_fullname_from_code $projectCode)"
@@ -34,7 +40,11 @@ function delete_pootle_project() {
         -d "form-0-DELETE=on" -d "changeprojects=Save Changes" \
         "$PO_SRV$path/admin/projects.html"
 	check_command
-	close_pootle_session
+	if [[ ${open_session+1} ]]; then
+		logt 3 "Keeping existing pootle session"
+	else
+		close_pootle_session
+	fi;
 
 	# seems that above post does not delete files on disk. Let's do it
 	logt 3 -n "Deleting project files on disk"
