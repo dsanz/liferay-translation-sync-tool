7e1175c269394ff08e13c43d9f84a4a28b893bba
dsanz <daniel.sanz@liferay.com>
2015-10-03 02:37:14 +0200

    use *_BIN variables to refer to actual executables. Assume env put them there for us

--- src/api/api-mail.sh
+++ src/api/api-mail.sh
@@ -2,11 +2,11 @@ function send_email() {
 	command="$0 $@";
 
 	echo "Command: $command <br>" > /tmp/body.html
-	cat $logfile | $ANSIFILTER_HOME/ansifilter -H -e UTF-8 -w256 -F monospace | sed 's/font-family/background:#000000;font-family/g' >> /tmp/body.html
+	cat $logfile | $ANSIFILTER_BIN -H -e UTF-8 -w256 -F monospace | sed 's/font-family/background:#000000;font-family/g' >> /tmp/body.html
 	rm /tmp/log.tgz > /dev/null 2>&1
 	tar czvf /tmp/log.tgz $logbase > /dev/null 2>&1
 
-	$SWAKS_HOME/swaks -t daniel.sanz@liferay.com \
+	$SWAKS_BIN -t daniel.sanz@liferay.com \
 		--from "sync-tool-no-reply@liferay.com" \
 		--header "Subject: $product $(date)" \
 		--add-header "Content-Type: text/html ; charset=\"UTF-8\"" \
