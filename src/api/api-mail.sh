913334f7d26bd5d6009178fcd0aa7229b7bb1f0b
dsanz <daniel.sanz@liferay.com>
2015-10-13 15:12:44 +0200

    some big improvements to the email functionality: compress data, clean dirs, make it clear the env where command runs, log activity...

--- src/api/api-mail.sh
+++ src/api/api-mail.sh
@@ -1,17 +1,29 @@
 function send_email() {
 	command="$0 $@";
 
+	logt 1 "Sending email with job details"
+	logt 2 "Packaging (this will be the last line included in the package)"
 	echo "Command: $command <br>" > /tmp/body.html
-	cat $logfile | $ANSIFILTER_BIN -H -e UTF-8 -w256 -F monospace | sed 's/font-family/background:#000000;font-family/g' >> /tmp/body.html
-	rm /tmp/log.tgz > /dev/null 2>&1
-	tar czvf /tmp/log.tgz $logbase > /dev/null 2>&1
 
+	rm /tmp/body.html > /dev/null 2>&1
+	rm /tmp/body.html.bz2 > /dev/null 2>&1
+	cat $logfile | $ANSIFILTER_BIN -H -e UTF-8 -w256 -F monospace | sed 's/font-family/background:#000000;font-family/g' >> /tmp/body.html
+
+	rm /tmp/log.tar.bz2 > /dev/null 2>&1
+	rm /tmp/log.tar > /dev/null 2>&1
+	tar cvf /tmp/log.tar $logbase > /dev/null 2>&1
+
+	bzip2 /tmp/log.tar
+	bzip2 /tmp/body.html
+
+	logt 2 -n "Sending "
 	$SWAKS_BIN -t daniel.sanz@liferay.com \
 		--from "sync-tool-no-reply@liferay.com" \
-		--header "Subject: $product $(date)" \
+		--header "Subject: ($LR_TRANS_MGR_PROFILE) $product $(date)" \
 		--add-header "Content-Type: text/html ; charset=\"UTF-8\"" \
 		--add-header "MIME-Version: 1.0" \
 		--body "Sync Tool execution: $command" \
-		--attach-type "text/html" --attach /tmp/body.html \
-		--attach /tmp/log.tgz > /dev/null 2>&1
+		--attach-type "text/html" --attach /tmp/body.html.bz2 \
+		--attach /tmp/log.tar.bz2 > /dev/null 2>&1
+	check_command
 }
