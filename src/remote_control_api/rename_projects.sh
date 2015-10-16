4816710148225df319fdf0f40fc5a7610cbd382c
dsanz <daniel.sanz@liferay.com>
2015-10-16 11:22:22 +0200

    rename some more projects

---
+++ src/remote_control_api/rename_projects.sh
@@ -0,0 +1,8 @@
+export HOME_DIR="$(dirname $(readlink -f $BASH_SOURCE))"
+
+. $HOME_DIR/base_env.sh
+
+run_sync_tool "-m" "portal" "portal-impl"
+run_sync_tool "-m" "ddl-form-portlet" "dynamic-data-lists-form-web"
+run_sync_tool "-m" "social-networking-portlet" "social-networking-web"
+run_sync_tool "-m" "marketplace-portlet" "marketplace-store-web"
