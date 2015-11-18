5c11a8245a12067fd79ed73fd011bda1d6c5cb86
dsanz <daniel.sanz@liferay.com>
2015-11-18 17:19:07 +0100

    improve display projects pretty print (VI)

--- src/api/api-project-provisioning.sh
+++ src/api/api-project-provisioning.sh
@@ -119,9 +119,9 @@ function display_AP_projects() {
 		project_list="$(echo ${AP_PROJECTS_BY_GIT_ROOT["$git_root"]} | sed 's: :\n:g' | sort)"
 		projects=$(echo "$project_list" | wc -l)
 		logt 2 "Git root: $git_root ($projects projects). Sync branch: ${GIT_ROOTS[$git_root]}. Reviewer: ${PR_REVIEWER[$git_root]}"
-		loglc 6 $RED "$(printf "%-51s%s" "Project Code")$(printf "%-100s%s" "Build-lang path (relative to git_root)")$(printf "%-35s%s" "Lang file base (rel. to build lang)")$(printf "%-60s%s" "Project name")$(printf "%-5s%s" "Check") "
+		logc $RED "$(printf "%-51s%s" "Project Code")$(printf "%-100s%s" "Build-lang path (relative to git_root)")$(printf "%-35s%s" "Lang file base (rel to build lang)")$(printf "%-60s%s" "Project name")$(printf "%-5s%s" "Check") "
 		while read project; do
-			logt 3 -n "$(printf "%-51s%s" "$project")"
+			logc $YELLOW -n "$(printf "%-51s%s" "$project")"
 			build_lang_path="${AP_PROJECT_BUILD_LANG_DIR[$project]}"
 			build_lang_path="${build_lang_path#$git_root}"
 			log -n "$(printf "%-100s%s" "$build_lang_path")"
@@ -130,10 +130,11 @@ function display_AP_projects() {
 			src_lang_base="${src_lang_base#$build_lang_path}"
 			log -n "$(printf "%-35s%s" "$src_lang_base")"
 			project_name="${AP_PROJECT_NAMES[$project]}"
-			log -n "$(printf "%-60s%s" "$project_name")"
+			logc $CYAN -n "$(printf "%-60s%s" "$project_name")"
 			[ -d ${AP_PROJECT_SRC_LANG_BASE[$project]} ]
 			check_command
 		done <<< "$project_list"
+		log
 	done;
 }
 
