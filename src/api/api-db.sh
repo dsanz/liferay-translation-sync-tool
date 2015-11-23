b7b069eeef393da596d337b79bb2f4b7f82533eb
dsanz <daniel.sanz@liferay.com>
2015-11-23 17:06:43 +0100

    make sure we skip the artificial locale called "templates", stored by pootle

--- src/api/api-db.sh
+++ src/api/api-db.sh
@@ -192,6 +192,6 @@ function get_pootle_project_codes() {
 
 function get_default_project_locales() {
 	local i=$($MYSQL_COMMAND $DB_NAME -s -N -e "select code from pootle_app_language where id in (select language_id from pootle_app_translationproject where real_path='${PORTAL_PROJECT_ID}');")
-	echo -e "$i";
+	echo -e "$i" | grep -v "templates";
 }
 
