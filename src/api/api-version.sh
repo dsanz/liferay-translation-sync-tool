d1ca61f6f3c3fca9419a7ad2c1ff8e6091f63459
dsanz <daniel.sanz@liferay.com>
2016-02-09 13:43:40 +0100

    Update version to 3.0RC7

--- src/api/api-version.sh
+++ src/api/api-version.sh
@@ -1,4 +1,4 @@
-declare -gr version="3.0RC6"
+declare -gr version="3.0RC7"
 declare -gr product_name="Liferay Translation Sync Tool"
 declare -gr old_product_name="Liferay translation manager"
 declare -gr product="$product_name v.$version"
