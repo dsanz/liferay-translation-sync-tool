fab119479f3f82f310618fc171210ae6060c0d9f
dsanz <daniel.sanz@liferay.com>
2016-03-04 10:28:01 +0100

    simplify api-files so that all its functions are called from api-backporter and only from there

--- src/backporter-api/api-files.sh
+++ src/backporter-api/api-files.sh
@@ -12,9 +12,30 @@ declare -g source_dir
 declare -g target_dir
 declare -g lang_file;
 
-#declare translations_dir="/portal-impl/src/content"
+function read_english_files() {
+	logt 3 "Reading english files"
+	source_english_path="$source_dir/${FILE}.${PROP_EXT}"
+	target_english_path="$target_dir/${FILE}.${PROP_EXT}"
+	read_locale_file $source_english_path $new_english
+	read_locale_file $target_english_path $old_english true
+}
 
-function compute_locales() {
+function read_lang_files() {
+	logt 3 "Reading translation files"
+	lang_file="${FILE}${LANG_SEP}$1.${PROP_EXT}";
+	source_lang_path="$source_dir/$lang_file"
+	target_lang_path="$target_dir/$lang_file"
+	read_locale_file $source_lang_path $new_lang
+	read_locale_file $target_lang_path $old_lang
+}
+
+function prepare_dirs() {
+	logt 3 "Preparing working dirs"
+	source_dir=$1
+	target_dir=$2
+	logt 4 "Source dir: $source_dir"
+	logt 4 "Target dir: $target_dir"
+
 	cd $target_dir
 	logt 4 -n "Computing locales for $target_dir"
 	for language_file in $(ls ${FILE}${LANG_SEP}*.$PROP_EXT); do
@@ -23,52 +44,6 @@ function compute_locales() {
 	check_command
 	locales="${L[@]}"
 	logt 4 "Locales in target dir: $locales"
-}
-
-function read_english_files() {
-	logt 3 "Reading english files"
-	set_english_paths
-	read_locale_file $source_english_path $new_english
-	read_locale_file $target_english_path $old_english true
-}
-
-function read_lang_files() {
-	logt 3 "Reading translation files"
-	set_lang_paths $1
-	read_locale_file $source_lang_path $new_lang
-	read_locale_file $target_lang_path $old_lang
-}
-
-function set_base_paths() {
-	source_dir=$1
-	target_dir=$2
-	#	if ! [[ ($source_dir == *$translations_dir*) || -f $source_dir/$english_file ]]; then
-	#		source_dir=$source_dir$translations_dir
-	#	fi;
-	#	if ! [[ $target_dir == *$translations_dir* || -f $target_dir/$english_file ]]; then
-	#		target_dir=$target_dir$translations_dir
-	#	fi;
-	logt 4 "Source dir: $source_dir"
-	logt 4 "Target dir: $target_dir"
-}
-
-# sets source and target paths for Language.properties files
-function set_english_paths() {
-	source_english_path="$source_dir/${FILE}.${PROP_EXT}"
-	target_english_path="$target_dir/${FILE}.${PROP_EXT}"
-}
-
-# sets source and target paths for Language_$1.properties files
-function set_lang_paths() {
-	lang_file="${FILE}${LANG_SEP}$1.${PROP_EXT}";
-	source_lang_path="$source_dir/$lang_file"
-	target_lang_path="$target_dir/$lang_file"
-}
-
-function prepare_dirs() {
-	logt 3 "Preparing working dirs"
-	set_base_paths $1 $2
-	compute_locales
 }
 
 function get_ee_target_dir() {
