1f0b015e71bbf3229b03480e18a8aa0936482850
dsanz <daniel.sanz@liferay.com>
2016-03-06 05:32:06 +0100

    create basic algorithm for merging pootle projects

---
+++ src/api/pootle/project/api_pootle_project_merge.sh
@@ -0,0 +1,35 @@
+function merge_pootle_projects() {
+	target_project_code="$1"
+	source_project_codes="$2"
+	logt 1 "Merging projects: $source_project_codes"
+	logt 1 "into project: $target_project_code"
+
+	read_pootle_projects_and_locales
+
+	prepare_output_dir "$target_project_code"
+
+	while read project; do
+		logt 2 "Processing source pootle project: $project"
+		# clean output dirs for source project code
+		prepare_output_dir "$project"
+
+		# export all source project files: sync_store or dump_store?
+		for locale in "${POOTLE_PROJECT_LOCALES[@]}"; do
+			language_file=$(get_file_name_from_locale $locale);
+			storeFile="$TMP_PROP_OUT_DIR/$project/$language_file.store"
+			dump_store "$project" "$locale" "$storeFile"
+			cat $storeFile >> $PODIR/$target_project_code/$language_file
+		done;
+
+		regenerate_file_stores "$project"
+		cat $PODIR/$project/$FILE.$PROP_EXT >> $PODIR/$target_project_code/$FILE.$PROP_EXT
+	done <<< "$source_project_codes"
+
+	# process translations for each language:
+	#   skip automatic copy/translations: publish translations will take care
+	#   skip english value unless DB contains it: dump store should take care (TODO: double check!!)
+
+	# provision new project (not from sources!)
+	provision_full_project_base $target_project_code $target_project_code  $PODIR/$target_project_code/
+
+}
