cd783fb21706960335c59d312c3cc4b402650058
dsanz <daniel.sanz@liferay.com>
2016-03-08 14:54:06 +0100

    add an initial, incomplete impl for refill translations in the repo-based way

--- src/api/sync/to_source/api_sync_to_source.sh
+++ src/api/sync/to_source/api_sync_to_source.sh
@@ -67,8 +67,7 @@ function process_project_translations_repo_based() {
 					read_pootle_store $target_project $language
 					read_ext_language_file $target_project $language
 
-					# TODO: reimplement this
-					#refill_translations $target_project $language $publish_translations
+					refill_translations_repo_based $source_project $target_project $language $publish_translations
 
 					logt 3 -n "Garbage collection (target: $target_project, $locale)... "
 					clear_keys "$(get_previous_language_prefix $target_project $locale)"
@@ -91,6 +90,147 @@ function process_project_translations_repo_based() {
 	check_command
 }
 
+
+function refill_translations_repo_based() {
+	set -f
+	source_project="$1";
+	target_project="$2";
+	language="$3";
+	publish_translations="$4"
+
+	locale=$(get_locale_from_file_name $language)
+
+	# involved file paths
+	srcfile="${AP_PROJECT_SRC_LANG_BASE["$project"]}/$language"
+	workingfile="${srcfile}.final"
+	copyingLogfile="$logbase/$project/$language"
+	conflictsLogPootle="$logbase/$project/$language.conflicts.pootle"
+	conflictsLogLiferay="$logbase/$project/$language.conflicts.liferay"
+
+	[[ -f $workingfile ]] && rm $workingfile # when debugging we don't run all sync stages so we can have this file from a previous run
+	target_lang_file_path="$TMP_PROP_OUT_DIR/$project/$language"
+
+	# prefixes for array accessing
+	exportedPrefix=$(get_exported_language_prefix $project $locale)
+	previousPrefix=$(get_previous_language_prefix $project $locale)
+	templatePrefix=$(get_template_prefix $project $locale)
+	storePrefix=$(get_store_language_prefix $project $locale)
+	extPrefix=$(get_ext_language_prefix $project $locale)
+
+	declare -A R  # reverse translations
+	declare -A Cp  # conflicts - pootle value
+	declare -A Cl  # conflicts - liferay source value
+
+	logt 3 "Copying translations..."
+	logt 0
+	done=false;
+	format="%s\n";
+	OLDIFS=$IFS
+	IFS=
+	until $done; do
+		if ! read -r line; then
+			done=true;
+			format="%s"
+		fi;
+		if [ ! "$line" == "" ]; then
+			char="!"
+			if is_key_line "$line" ; then
+				[[ "$line" =~ $kv_rexp ]] && key="${BASH_REMATCH[1]}" && value="${BASH_REMATCH[2]}"
+				valueTpl=${T["$templatePrefix$key"]}
+				valueExp=${T["$exportedPrefix$key"]}                            # get translation exported by pootle
+
+				if exists_ext_value $extPrefix $key; then                       # has translation to be overriden?
+					value=${T["$extPrefix$key"]}
+					char="o"
+				elif [[ "$valueExp" == "$valueTpl" ]]; then                     # ok, no overriding. Now, is exported value = template value?
+					valueStore=${T["$storePrefix$key"]}                         #   then let's see if translators wrote the template value by hand in the text box
+					if [[ "$valueStore" == "$valueTpl" ]]; then                 #   was it translated that way on purpose?
+						char="e"                                                #       use the template value. English is ok in this case.
+						value=$valueTpl
+					else                                                        #   otherwise, key is really untranslated in pootle
+						valuePrev=${T["$previousPrefix$key"]}                   #       let's look for the current key translation in master
+						if is_translated_value "$valuePrev"; then               #       is the key translated in master? [shouldn't happen unless we run -r before a -p]
+							if [[ "$valuePrev" != "$valueTpl" ]]; then          #           ok, key is already translated in master. is that value different from the template?
+								char="r"                                        #               ok, then master is translated but Pootle not, hmmm! we have a reverse-path
+								value="$valuePrev"                              #               let's keep`the value in master as a good default.
+								R[$key]="$value";                               #               and memorize it so that Pootle can be properly updated later
+							else                                                #           ok, value in master is just like the template
+								char="a"                                        #               discard it! ant build-lang will do
+								value=""
+							fi
+						else                                                    #       value in master is not translated. This means an auto-copy or auto-translation
+							char="u"                                            #           let's reuse it, we are saving build-lang work. don't do same work twice
+							value=$valuePrev
+						fi;
+					fi
+				else                                                            # ok, no overriding, and value is not the english one: it's supposed to be a valid translation!!
+					value=${T["$exportedPrefix$key"]}                           #   get translation exported by pootle
+					valuePrev=${T["$previousPrefix$key"]}                       #   get the translation from master
+					if is_translated_value "$valuePrev"; then                   #   is the master value translated?
+						if [[ "$valuePrev" != "$value" ]]; then                 #      is this translation different than the one pootle exported?
+							char="x"                                            #           ok, we have a conflict, pootle wins. Let user know
+							Cp[$key]="$value"                                   #           take note for logging purposes
+							Cl[$key]="$valuePrev"
+						else                                                    #      ok, translation in master is just like the exported by pootle.
+							char="·"                                            #           no-op, already translated both in pootle and master
+						fi
+					else                                                        #   master value is NOT translated but auto-translated/auto-copied
+						char="p"                                                #      ok, translated in pootle, but not in master. OK!!
+					fi
+				fi
+				result="${key}=${value}"
+			else                                                               # is it a comment line?
+				char="#"
+				result="$line"                                                 #    get the whole line
+			fi
+			printf "$format" "$result" >> $workingfile
+			printf "$format"  "[${char}]___${key}" >> $copyingLogfile
+			loglc 0 "${charc[$char]}" -n "$char"
+		fi;
+	done < $target_lang_file_path
+	IFS=$OLDIFS
+
+	logt 0
+	if [[ "$publish_translations" == true ]]; then
+		if [[ ${#R[@]} -gt 0 ]];  then
+			# required by api-db to access pootle DB in case we need to know if a term was translated using the english word or not
+			storeId=$(get_store_id $source_project $locale)
+			local path=$(get_pootle_path $source_project $locale)
+
+			logt 3 "Submitting translations from source to pootle. Next time be sure to run this manager with -p option!"
+			start_pootle_session
+			for key in "${!R[@]}"; do
+				value="${R[$key]}"
+				upload_submission "$key" "$value" "$storeId" "$path"
+			done;
+			close_pootle_session
+		fi
+	else
+		logt 3 "Translation submission is disabled"
+	fi;
+	if [[ ${#Cp[@]} -gt 0 ]]; then
+		logt 3 "Conflicts warning:"
+		logt 4 "Conflicts are keys having correct, different translations both in pootle and in liferay sources. During pootle2src, the pootle value will be considered the correct one"
+		logt 4 "Please compare contents of following files:"
+		logt 5 "$conflictsLogPootle"
+		logt 5 "$conflictsLogLiferay"
+		logt 4 -n "Generating conflict files"
+		for key in "${!Cp[@]}"; do
+			printf "%s=%s" "$key" "${Cp[$key]}" >> $conflictsLogPootle
+			printf "%s=%s" "$key" "${Cl[$key]}" >> $conflictsLogLiferay
+		done;
+		check_command
+	fi
+	log
+	set +f
+	unset R
+	unset Cp
+	unset Cl
+	logt 3 "Moving processed file to source dir"
+	logt 4 -n "Moving to $srcfile"
+	mv $workingfile $srcfile
+	check_command
+}
 
 #
 # Traditional, module-based sync logic
