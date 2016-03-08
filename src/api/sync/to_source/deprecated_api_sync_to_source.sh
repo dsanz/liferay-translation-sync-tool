c3f3dc64bac587160b8e7034221c7c16ccd74772
dsanz <daniel.sanz@liferay.com>
2016-03-08 15:16:21 +0100

    move deprecated sync functions

--- src/api/sync/to_source/deprecated_api_sync_to_source.sh
+++ src/api/sync/to_source/deprecated_api_sync_to_source.sh
@@ -1,89 +1,38 @@
-#!/bin/bash
-
-# pootle2src implements the sync between pootle and liferay source code repos
-# - tells pootle to update its files with the DB contents
-# - copy and convert those files to utf-8
-# - pulls from upstream the master branch
-# - process translations by comparing pootle export with contents in master, using a set of predefined rules
-# - makes a first commit of this
-# - runs ant build-lang for every project
-# - commits and pushes the result
-# - creates a Pull Request for each involved git root, sent to a different reviewer.
-# all the process is logged
-function pootle2src() {
-	loglc 1 $RED "Begin Sync[Pootle -> Liferay source code]"
-	display_source_projects_action
-	clean_temp_output_dirs
-	export_pootle_translations_to_temp_dirs
-	restore_file_ownership
-	process_translations_repo_based
-	do_commit false false "Translations sync from translate.liferay.com"
-	build_lang
-	do_commit true true "build-lang"
-	loglc 1 $RED "End Sync[Pootle -> Liferay source code]"
-}
-
 #
-# new, repository-based sync logic
+# Traditional, module-based sync logic
 #
 
-function process_translations_repo_based() {
-	logt 1 "Processing translations for export"
-
-	# TODO: print whatever legend we have
-
-	for git_root in "${!GIT_ROOTS[@]}"; do
-		process_project_translations_repo_based $git_root true
-	done;
-}
-
-# process translations from a repo-based
-function process_project_translations_repo_based() {
-	git_root="$1"
-
-	source_project="${GIT_ROOT_POOTLE_PROJECT_NAME[$git_root]}"
-	target_project_list="$(echo ${AP_PROJECTS_BY_GIT_ROOT["$git_root"]} | sed 's: :\n:g' | sort)"
-	languages=`ls $PODIR/$source_project`
-
-	logt 2 "$source_project"
+function process_project_translations() {
+	project="$1"
+	publish_translations="$2"
+	languages=`ls $PODIR/$project`
+	logt 2 "$project"
 	logt 3 "Setting up per-project log file"
-	check_dir "$logbase/$source_project/"
-
-	# this has to be read once per source project
-	read_pootle_exported_template $source_project
-
+	check_dir "$logbase/$project/"
+	logt 3 "Reading template file"
+	read_pootle_exported_template $project
 	for language in $languages; do
 		locale=$(get_locale_from_file_name $language)
 		if [[ "$locale" != "en" && "$language" =~ $trans_file_rexp ]]; then
-			logt 2 "$source_project: $locale"
-
-			# these have to be read once per source project and language
-			read_pootle_exported_language_file $source_project $language
-			read_pootle_store $source_project $language
-			read_ext_language_file $source_project $language
-
-			# iterate all projects in the destination project list and 'backport' to them
-			while read target_project; do
-				if [[ $target_project != $source_project ]]; then
-					# this has to be read once per target project and locale
-					read_source_code_language_file $target_project $language
-
-					refill_translations_repo_based $source_project $target_project $language $publish_translations
-
-					logt 3 -n "Garbage collection (target: $target_project, $locale)... "
-					clear_keys "$(get_source_code_language_prefix $target_project $locale)"
-					check_command
-				fi
-			done <<< "$target_project_list"
-
-			logt 3 -n "Garbage collection (source: $source_project, $locale)... "
-			clear_keys "$(get_exported_language_prefix $source_project $locale)"
-			clear_keys "$(get_store_language_prefix $source_project $locale)"
-			clear_keys "$(get_ext_language_prefix $source_project $locale)"
+			logt 2 "$project: $locale"
+			logt 3 "Reading $language file"
+			read_pootle_exported_language_file $project $language
+			logt 3 "Reading $language file from source code branch (just pulled)"
+			read_source_code_language_file $project $language
+			logt 3 "Reading pootle store"
+			read_pootle_store $project $language
+			logt 3 "Reading overriding translations"
+			read_ext_language_file $project $language
+			refill_translations $project $language $publish_translations
+			logt 3 -n "Garbage collection... "
+			clear_keys "$(get_exported_language_prefix $project $locale)"
+			clear_keys "$(get_source_code_language_prefix $project $locale)"
+			clear_keys "$(get_store_language_prefix $project $locale)"
+			clear_keys "$(get_ext_language_prefix $project $locale)"
+			check_command
 		fi
 	done
-
-	logt 3 -n "Garbage collection (source project: $source_project)... "
+	logt 3 -n "Garbage collection (whole project)... "
 	unset K
 	unset T
 	declare -gA T;
@@ -92,44 +41,77 @@ function process_project_translations_repo_based() {
 }
 
 
-function refill_translations_repo_based() {
-	set -f
-	source_project="$1";
-	target_project="$2";
-	language="$3";
-	publish_translations="$4"
+# Pootle exports all untranslated keys, assigning them the english value. This function restores the values in old version of Language_*.properties
+# this way, untranslated keys will have the Automatic Copy/Translation tag
+function process_translations() {
+	logt 1 "Processing translations"
+	logt 2 "Legend:"
+	unset charc
+	unset chart
+	declare -gA charc # colors
+	declare -gA chart # text legend
+	charc["!"]=$RED; chart["!"]="uncovered case"
+	charc["o"]=$WHITE; chart["o"]="overriden from ext file"
+	charc["e"]=$RED; chart["e"]="English value is ok, was translated on purpose using Pootle"
+	charc["r"]=$YELLOW; chart["r"]="reverse-path (sources translated, pootle is untranslated). Will be published to Pootle"
+	charc["a"]=$CYAN; chart["a"]="ant build-lang will do (sources and pootle untranslated)"
+	charc["u"]=$BLUE; chart["u"]="untranslated, pick existing source value (Pootle untranslated, source auto-translated or auto-copied)"
+	charc["x"]=$LILA; chart["x"]="conflict/improvement Pootle wins (pootle and sources translated, different values). Review $copyingLogfile "
+	charc["·"]=$COLOROFF; chart["·"]="no-op (same, valid translation in pootle and sources)"
+	charc["p"]=$GREEN; chart["p"]="valid translation coming from pootle, sources untranslated"
+	charc["#"]=$COLOROFF; chart["#"]="comment/blank line"
+	for char in ${!charc[@]}; do
+		loglc 8 ${charc[$char]} "'$char' ${chart[$char]}.  "
+	done;
 
+	for project in "${POOTLE_PROJECT_CODES[@]}"; do
+		process_project_translations $project true
+	done
+}
+
+# Due to pootle exports all untranslated keys, there is no way to know if a value in Language_xx.properties
+# comes from an untranslated keys or a key which valid value in that language is the same than english.
+# This distinction is crucial to skip auto translating terms which are better known if the english word is used.
+# For example: "staging" is used in Spanish to denote "Staging".
+# In order to know this, direct access to Pootle DB is needed
+function refill_translations() {
+	set -f
+	project="$1";
+	language="$2";
+	publish_translations="$3"
 	locale=$(get_locale_from_file_name $language)
 
+	# required by api-db to access pootle DB in case we need to know if a term was translated using the english word or not
+	storeId=$(get_store_id $project $locale)
+	local path=$(get_pootle_path $project $locale)
+
 	# involved file paths
-	target_file="${AP_PROJECT_SRC_LANG_BASE["$target_project"]}/$language"
-	workingfile="${target_file}.final"
+	srcfile="${AP_PROJECT_SRC_LANG_BASE["$project"]}/$language"
+	workingfile="${srcfile}.final"
 	copyingLogfile="$logbase/$project/$language"
 	conflictsLogPootle="$logbase/$project/$language.conflicts.pootle"
 	conflictsLogLiferay="$logbase/$project/$language.conflicts.liferay"
 
 	[[ -f $workingfile ]] && rm $workingfile # when debugging we don't run all sync stages so we can have this file from a previous run
+	target_lang_file_path="$TMP_PROP_OUT_DIR/$project/$language"
 
-	# source project prefixes for array accessing
-	templatePrefix=$(get_template_prefix $source_project $locale)
-	exportedPrefix=$(get_exported_language_prefix $source_project $locale)
-	storePrefix=$(get_store_language_prefix $source_project $locale)
-	extPrefix=$(get_ext_language_prefix $source_project $locale)
-
-	# target project prefixes for array accessing
-	sourceCodePrefix=$(get_source_code_language_prefix $target_project $locale)
+	# prefixes for array accessing
+	exportedPrefix=$(get_exported_language_prefix $project $locale)
+	sourceCodePrefix=$(get_source_code_language_prefix $project $locale)
+	templatePrefix=$(get_template_prefix $project $locale)
+	storePrefix=$(get_store_language_prefix $project $locale)
+	extPrefix=$(get_ext_language_prefix $project $locale)
 
 	declare -A R  # reverse translations
 	declare -A Cp  # conflicts - pootle value
 	declare -A Cl  # conflicts - liferay source value
 
-	logt 3 "Exporting translations from $source_project to $target_project"
+	logt 3 "Copying translations..."
 	logt 0
 	done=false;
 	format="%s\n";
 	OLDIFS=$IFS
 	IFS=
-	# read the target language file
 	until $done; do
 		if ! read -r line; then
 			done=true;
@@ -190,16 +172,12 @@ function refill_translations_repo_based() {
 			printf "$format"  "[${char}]___${key}" >> $copyingLogfile
 			loglc 0 "${charc[$char]}" -n "$char"
 		fi;
-	done < $target_file
+	done < $target_lang_file_path
 	IFS=$OLDIFS
 
 	logt 0
 	if [[ "$publish_translations" == true ]]; then
 		if [[ ${#R[@]} -gt 0 ]];  then
-			# required by api-db to access pootle DB in case we need to know if a term was translated using the english word or not
-			storeId=$(get_store_id $source_project $locale)
-			local path=$(get_pootle_path $source_project $locale)
-
 			logt 3 "Submitting translations from source to pootle. Next time be sure to run this manager with -p option!"
 			start_pootle_session
 			for key in "${!R[@]}"; do
@@ -230,7 +208,7 @@ function refill_translations_repo_based() {
 	unset Cp
 	unset Cl
 	logt 3 "Moving processed file to source dir"
-	logt 4 -n "Moving to $target_file"
-	mv $workingfile $target_file
+	logt 4 -n "Moving to $srcfile"
+	mv $workingfile $srcfile
 	check_command
 }
