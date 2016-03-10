1492244e0fcb252eeea64861496af6469b9a01fc
dsanz <daniel.sanz@liferay.com>
2016-03-10 18:07:17 +0100

    add a single, bidirectional sync functions taking logic both from src2pootle and pootle2src, which turned out to be quite similar

---
+++ src/api/sync/api_sync.sh
@@ -0,0 +1,59 @@
+function sync() {
+	loglc 1 $RED "Begin Synchronization"
+	display_source_projects_action
+	create_backup_action
+	update_pootle_db_from_templates_repo_based
+	clean_temp_input_dirs
+	clean_temp_output_dirs
+	restore_file_ownership
+
+	# merge
+	sync_translations
+
+	do_commit false false "Translations sync from translate.liferay.com"
+	build_lang
+	do_commit true true "build-lang"
+
+	restore_file_ownership
+	refresh_stats_repo_based
+
+	loglc 1 $RED "End Synchronization"
+}
+
+
+function sync_translations() {
+	logt 1 "Synchronizing translations"
+
+	logt 2 "Legend:"
+	unset charc
+	unset chart
+	declare -gA charc # colors
+	declare -gA chart # text legend
+
+	# to pootle
+	charc["#"]=$COLOROFF; chart["#"]="comment/blank line"
+	charc["R"]=$YELLOW; chart["R"]="reverse-path (sources translated, pootle untranslated). Will be published to Pootle"
+	charc["u"]=$BLUE; chart["u"]="source code untranslated. Can not update pootle"
+	charc["-"]=$WHITE; chart["-"]="source code has a translation which key no longer exists. Won't update pootle"
+	charc["·"]=$GREEN; chart["·"]="no-op (same, valid translation in pootle and sources)"
+
+	# to sources
+	charc["!"]=$RED; chart["!"]="uncovered case"
+	charc["o"]=$WHITE; chart["o"]="overriden from ext file"
+	charc["e"]=$RED; chart["e"]="English value is ok, was translated on purpose using Pootle"
+	charc["r"]=$YELLOW; chart["r"]="reverse-path (sources translated, pootle is untranslated). Will be published to Pootle"
+	charc["a"]=$CYAN; chart["a"]="ant build-lang will do (sources and pootle untranslated)"
+	charc["u"]=$BLUE; chart["u"]="untranslated, pick existing source value (Pootle untranslated, source auto-translated or auto-copied)"
+	charc["x"]=$LILA; chart["x"]="conflict/improvement Pootle wins (pootle and sources translated, different values). Review $copyingLogfile "
+	charc["·"]=$COLOROFF; chart["·"]="no-op (same, valid translation in pootle and sources)"
+	charc["p"]=$GREEN; chart["p"]="valid translation coming from pootle, sources untranslated"
+	charc["#"]=$COLOROFF; chart["#"]="comment/blank line"
+
+	for char in ${!charc[@]}; do
+		loglc 8 ${charc[$char]} "'$char' ${chart[$char]}.  "
+	done;
+
+	for git_root in "${!GIT_ROOTS[@]}"; do
+		sync_project_translations $git_root
+	done;
+}
