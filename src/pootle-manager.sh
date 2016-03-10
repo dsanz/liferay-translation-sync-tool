6fd5b0368a1532baaec4c173566c8a5d6ede7153
dsanz <daniel.sanz@liferay.com>
2016-03-10 23:25:23 +0100

    update imports

--- src/pootle-manager.sh
+++ src/pootle-manager.sh
@@ -30,12 +30,11 @@ function load_api() {
 
 	. api/sync/backport/api_sync_backport.sh
 	. api/sync/backport/api_sync_backport_git.sh
-	. api/sync/to_source/api_sync_to_source.sh
+	. api/sync/api_sync.sh
 	. api/sync/to_source/api_sync_to_source_build_sources.sh
-	. api/sync/to_source/api_sync_to_source_lang_files.sh
+	. api/sync/api_sync_lang_files.sh
 	. api/sync/to_source/api_sync_to_source_git.sh
 	. api/sync/to_pootle/api_sync_to_pootle.sh
-	. api/sync/to_pootle/api_sync_to_pootle_lang_files.sh
 
 	# Load Actions
 	. actions/backup/create_backup_action.sh
