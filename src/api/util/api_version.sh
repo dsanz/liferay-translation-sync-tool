02065ddac1180a030f45a02a235ed654565be70d
dsanz <daniel.sanz@liferay.com>
2016-03-14 12:04:34 +0100

    update version to v.4.0.3

--- src/api/util/api_version.sh
+++ src/api/util/api_version.sh
@@ -1,4 +1,4 @@
-declare -gr version="4.0.2"
+declare -gr version="4.0.3"
 declare -gr product_name="Liferay Translation Sync Tool"
 declare -gr old_product_name="Liferay translation manager"
 declare -gr product="$product_name v.$version"
