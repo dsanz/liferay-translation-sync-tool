fde960309519c110e65ec0babde478b9af3beced
dsanz <daniel.sanz@liferay.com>
2016-03-31 11:48:46 +0200

    avoid joining two lines in one when last line of Language.properties does not end in \n

--- src/api/pootle/sync/api_pootle_import.sh
+++ src/api/pootle/sync/api_pootle_import.sh
@@ -61,8 +61,8 @@ function update_pootle_db_from_templates() {
 	logt 3 "Combining templates of  ${#AP_PROJECT_NAMES[@]} into a single file"
 	for source_code_project in "${!AP_PROJECT_NAMES[@]}"; do
 		logt 4 -n "Adding $source_code_project template"
-		# TODO: make sure we have a new line after each cat even id source file does not
 		cat ${AP_PROJECT_SRC_LANG_BASE[$source_code_project]}/$FILE.$PROP_EXT >> $PODIR/$POOTLE_PROJECT_ID/$FILE.$PROP_EXT
+		echo >> $PODIR/$POOTLE_PROJECT_ID/$FILE.$PROP_EXT
 		check_command
 	done
 
